0ea28d558311ef0ea7932bf58f663329bad0ed9c
Shahmir-saleem <157698728+Shahmir-saleem@users.noreply.github.com>
2024-05-31 23:23:32 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,87 @@
+// var arr = []
+
+
+// var arr_string = ["aman", "daniyal","raza","afaq"]
+
+
+// var arr_num = [12,11,25,26]
+
+
+// var arr_boolean = [true,false]
+
+
+// var arr_mixed = ["hassan",55,false]
+
+
+
+// document.write('Education qualifications in Pakistan' , '<br>', '<br>')
+// var edu = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M. Phil","PhD"]
+// for(var i =0; i<edu.length; i++){
+//     document.write(i+1+" ",edu[i] , "<br>" ,  '<br>')
+// }
+
+
+//  document.write('Students Score' , '<br>', '<br>')
+// var student_arr = ["kamran","ammad","jhon"]
+// var student_score = [320,230,480]
+// document.write('Score of' + ' ' +student_arr[0] + ' ' +"is" + ' ' +student_score[0] + ' ' + "Percentage: 64%","<br>")
+// document.write('Score of' + ' ' +student_arr[1] + ' ' +"is" + ' ' +student_score[1] + ' ' + "Percentage: 46%","<br>")
+// document.write('Score of' + ' ' +student_arr[2] + ' ' +"is" + ' ' +student_score[2] + ' ' + "Percentage: 96%","<br>" , '<br>')
+
+
+// document.write('Color Assignment' , '<br>', '<br>')
+// var arr_color = ["red" ,"blue" ,"green" ,"grey" ,"white" ,"black"]
+// document.write(arr_color , '<br>' , '<br>')
+
+// document.write('Colors add to Start : unshift' , '<br>', '<br>')
+// arr_color.unshift("yellow")
+// document.write(arr_color , '<br>' , '<br>')
+
+// document.write('Colors add to end : push' , '<br>', '<br>')
+// arr_color.push("purple")
+// document.write(arr_color , '<br>' , '<br>')
+
+// document.write('Colors remove from start : shift' , '<br>', '<br>')
+// arr_color.shift()
+// document.write(arr_color , '<br>' , '<br>')
+
+// document.write('Colors remove from end : pop' , '<br>', '<br>')
+// arr_color.pop()
+// document.write(arr_color , '<br>' , '<br>'  )
+
+// document.write('Delete & add new color : splice' , '<br>', '<br>')
+// arr_color.splice(2,1,"orange")
+// document.write(arr_color , '<br>' , '<br>')
+
+
+
+// document.write('Score of Students' , '<br>', '<br>')
+// var studentScore = [320,230,480,120]
+
+// document.write(studentScore[3]+ ' ' +studentScore[1]+ ' ' +studentScore[0]+ ' ' +studentScore[2] , '<br>', '<br>')
+
+// document.write('Initialize city names' , '<br>', '<br>')
+// var cityNames = ["Karachi","Lahore","Islamabad","Quetta","Peshawar"]
+// document.write(cityNames , '<br>' , '<br>')
+// document.write('Selected cities list' , '<br>', '<br>')
+// var copy = cityNames.slice(2,4 )
+// document.write(copy , '<br>' , '<br>')
+
+// document.write('Array in single string' , '<br>', '<br>')
+// var arr_cat = ["This","is","my","cat"]
+// document.write(arr_cat , '<br>' , '<br>')
+// document.write('after join method' , '<br>', '<br>')
+// var join = arr_cat.join
+
+
+// document.write('Phone Manufacturers' , '<br>', '<br>')
+
+// var phone = ["Apple","Samsung", "Motorola", "Nokia", "Sony" , "Haier"]
+// document.write(`<select>
+// <option>${phone[0]}</option>
+// <option>${phone[1]}</option>
+// <option>${phone[2]}</option>
+// <option>${phone[3]}</option>
+// <option>${phone[4]}</option>
+// <option>${phone[5]}</option>
+// </select>` )
